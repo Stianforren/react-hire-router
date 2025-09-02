8669f7e6c1508a456d7baf5c01f7148f42cd0d1e
Stian Forren <stian.forren@experisacademy.no>
2025-09-02 15:00:28 +0200

    with css

--- src/pages/Dashboard/components/PeopleListItem.jsx
+++ src/pages/Dashboard/components/PeopleListItem.jsx
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom"
+import '../../../App.css'
 function PeopleListItem(props) {
   const { person } = props
 
   return (
     <article className="user_profile">
     <li>
+      <img src={person.picture.medium} alt={`${person.name.first} ${person.name.last}`} />
+      
       <h3 className="profile_name">
         <Link to={`view/${person.login.uuid}`}> {person.name.first} {person.name.last}</Link>
       </h3>
